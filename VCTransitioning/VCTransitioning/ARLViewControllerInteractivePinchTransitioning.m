d6b7a4b4031bc80bdbd114523d7ba9219eec2cb5
Andrew Slabko <andrew@slabko.ru>
2015-02-26 17:29:38 +0100

    Fixed animated transitioning

--- VCTransitioning/VCTransitioning/ARLViewControllerInteractivePinchTransitioning.m
+++ VCTransitioning/VCTransitioning/ARLViewControllerInteractivePinchTransitioning.m
@@ -94,7 +94,7 @@
 
 - (void)animateTransition:(id<UIViewControllerContextTransitioning>)context
 {
-    [self finishTransition];
+    [self startInteractiveTransition:context];
 }
 
 //----------------------------------------------------------------------------------------------------------------------
